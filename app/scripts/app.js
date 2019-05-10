5a57061ff74a3bb51b257d0990761f0d1a10393a
santilland <daniel.santillan@eox.at>
2019-05-10 14:43:08 +0200

    updated settings changed default clamping

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -232,7 +232,7 @@
 							for (var j = pKeys.length - 1; j >= 0; j--) {
 								p.parameters[pKeys[j]]["colorscale"] = defaultFor(p.parameters[pKeys[j]]["colorscale"], "viridis");
 								p.parameters[pKeys[j]]["clamp_min"] = defaultFor(p.parameters[pKeys[j]]["clamp_min"], false);
-								p.parameters[pKeys[j]]["clamp_max"] = defaultFor(p.parameters[pKeys[j]]["clamp_max"], true);
+								p.parameters[pKeys[j]]["clamp_max"] = defaultFor(p.parameters[pKeys[j]]["clamp_max"], false);
 							}
 						}
 
